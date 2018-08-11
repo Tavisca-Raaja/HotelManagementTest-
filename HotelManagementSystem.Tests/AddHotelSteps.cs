04c61283626ea753244f2df87eca0f6934ee3f8f
Raaja Gouthaman <rgouthaman@tavisca.com>
2018-08-11 22:10:24 +0530

    msg

---
+++ HotelManagementSystem.Tests/AddHotelSteps.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using HotelManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace HotelManagementSystem.Tests
+{
+    [Binding]
+    public class AddHotelSteps
+    {
+        private Hotel hotel = new Hotel();
+        private Hotel addHotelResponse;
+        private List<Hotel> hotels = new List<Hotel>();
+
+
+        [Given(@"User provided valid Id '(.*)'  and '(.*)'for hotel")]
+        public void GivenUserProvidedValidIdAndForHotel(int id, string name)
+        {
+            hotel.Id = id;
+            hotel.Name = name;
+        }
+
+        [Given(@"User has added required details for hotel")]
+        public void GivenUseHasAddedRequiredDetailsForHotel()
+        {
+            SetHotelBasicDetails();
+        }
+
+        [When(@"User calls AddHotel api")]
+        [Given(@"User called AddHotel api")]
+        public void WhenUserCallsAddHotelApi()
+        {
+            hotels = HotelsApiCaller.AddHotel(hotel);
+        }
+
+        [Then(@"Hotel with name '(.*)' should be present in the response")]
+        public void ThenHotelWithNameShouldBePresentInTheResponse(string name)
+        {
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response",name));
+        }
+        [When(@"User calls GetHotelById api by Id '(.*)'")]
+        public void WhenUserCallsGetHotelByIdApiById(int id)
+        {
+            hotel= HotelsApiCaller.GetHotelById(id);
+            
+        }
+
+
+        [Then(@"Hotel with given name '(.*)' should be present in the response")]
+        public void ThenHotelWithGivenNameShouldBePresentInTheResponse(string name)
+        {
+            hotel.Name.Should().Be(name);
+        }
+
+        [When(@"User calls GetAllHotels api")]
+        public void WhenUserCallsGetAllHotelsApi()
+        {
+            hotels = HotelsApiCaller.GetAllHotels();
+        }
+
+        [Then(@"Hotel with given names '(.*)' and '(.*)' and '(.*)' should be present in the response")]
+        public void ThenHotelWithGivenNamesAndAndShouldBePresentInTheResponse(string name, string name1, string name2)
+        {
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response", name));
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name1.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response", name1));
+            hotel = hotels.Find(htl => htl.Name.ToLower().Equals(name2.ToLower()));
+            hotel.Should().NotBeNull(string.Format("Hotel with name {0} not found in response", name2));
+        }
+
+
+
+
+        private void SetHotelBasicDetails()
+        {
+            hotel.ImageURLs = new List<string>() { "image1", "image2" };
+            hotel.LocationCode = "Location";
+            hotel.Rooms = new List<Room>() { new Room() { NoOfRoomsAvailable = 10, RoomType = "delux" } };
+            hotel.Address = "Address1";
+            hotel.Amenities = new List<string>() { "swimming pool", "gymnasium" };
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HotelManagementSystem.Tests/SpecFlowFeature1.feature
@@ -0,0 +1,53 @@
+﻿Feature: Add hotel
+	In order to simulate hotel management system
+	As api consumer
+	I want to be able to add hotel,get hotel details and book hotel
+
+@AddHotel
+Scenario Outline: User adds hotel in database by providing valid inputs
+	Given User provided valid Id '<id>'  and '<name>'for hotel 
+	And User has added required details for hotel
+	When User calls AddHotel api
+	Then Hotel with name '<name>' should be present in the response
+Examples: 
+| id | name   |
+| 1  | hotel1 |
+| 2  | hotel2 |
+| 3  | hotel3 |
+
+@GetHotelById
+Scenario Outline: User finds hotel in database by providing valid inputs
+    Given User provided valid Id '<id>'  and '<name>'for hotel 
+	And User has added required details for hotel
+    And User called AddHotel api
+	When User calls GetHotelById api by Id '<id>'
+	Then Hotel with given name '<name>' should be present in the response
+Examples: 
+| id | name   |
+| 4  | hotel1 |
+| 5  | hotel2 |
+| 6  | hotel3 |
+
+@GetAllHotels
+Scenario Outline: User add multiple hotels and find all hotels in database
+   Given User provided valid Id '<id>'  and '<name>'for hotel
+   And User has added required details for hotel
+   And User called AddHotel api
+   And User provided valid Id '<id1>'  and '<name1>'for hotel
+   And User has added required details for hotel
+   And User called AddHotel api
+   And User provided valid Id '<id2>'  and '<name2>'for hotel
+   And User has added required details for hotel
+   And User called AddHotel api
+   When User calls GetAllHotels api
+   Then Hotel with given names '<name>' and '<name1>' and '<name2>' should be present in the response
+
+   Examples: 
+| id | name   | id1 | name1  | id2 | name2  |
+| 7  | hotel1 | 8   | hotel2 | 9   | hotel3 |
+| 10 | hotel1 | 11  | hotel2 | 12  | hotel3 |
+| 13 | hotel1 | 14  | hotel2 | 15  | hotel3 |
+
+
+
+
